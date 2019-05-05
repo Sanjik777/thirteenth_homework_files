f56604a2588020939a6cbc4e9224762c0632cb55
Sanjik <Beiseev_193@mail.ru>
2019-05-06 01:23:32 +0600

    v1

---
+++ practical_work/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+/*
+ Взаимодействие с файловой системой.
+
+1.	Написать программу, читающую побайтно заданный файл и подсчитывающую 
+число появлений каждого из 256 возможных знаков.
+2.	С помощью класса StreamWriter записать в текстовый файл свое имя,
+фамилию и возраст. Каждая запись должна начинаться с новой строки.
+ */
+namespace practical_work
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("\n----------------------1---------------------\n");
+
+			string textRead = null;//для способа StreamReader
+			int counter = 0;
+
+			string filePath = "textFile.txt";
+			if (!File.Exists(filePath))
+			{
+				var stream = File.Create(filePath);
+				stream.Close();
+			}
+			else
+			{
+				var temp = new FileInfo(filePath);
+				temp.Delete();
+				var stream = File.Create(filePath);
+				stream.Close();
+			}
+
+			//Проверим длину изначальной строки
+			string textFile = "Asd@F)3449&GF*#NCV|{:>?>:L<:L<";
+			Console.WriteLine($"Проверка длины строки: {textFile.Length}\n");
+
+			//Способ чтения через StreamReader
+			using (StreamWriter fileWriter = new StreamWriter(filePath))
+			{
+				fileWriter.WriteLine(textFile);
+			};			
+			using (StreamReader fileReader = new StreamReader(filePath))
+			{
+				while (fileReader.Peek()>=0)
+				{
+					counter++;
+					textRead += (char)fileReader.Read();
+				}
+				//Второй способ чтения:
+				//fileReader.BaseStream.Position = 0;// возвращаем на начало
+				//textRead = fileReader.ReadToEnd(); // читаем целиком
+			};
+			Console.WriteLine($"Вывод символов из файла: {textRead}");
+			Console.WriteLine($"Кол-во символов в файле через StreamReader: {counter}");
+
+			//Способ чтения через FileStream
+			int secondCounter = 0;
+			using (FileStream fileStream = File.OpenRead(filePath))
+			{
+				for (int i=0; i < fileStream.Length; i++)
+				{
+					fileStream.ReadByte();
+					secondCounter++;
+				}
+			};
+			Console.WriteLine($"Кол-во символов в файле через FileStream: {secondCounter}");
+
+
+			Console.WriteLine("\n----------------------2---------------------\n");
+			string secondPath = "SecondFile.txt";
+			
+			using (StreamWriter fileWriter = new StreamWriter(secondPath))
+			{
+				fileWriter.WriteLine("Ivanov");
+				fileWriter.WriteLine("Ivan");
+				fileWriter.WriteLine("23");
+			};
+			using (StreamReader fileReader = new StreamReader(secondPath))
+			{
+				string dataPerson = fileReader.ReadToEnd();
+				Console.WriteLine(dataPerson);
+			};
+			Console.ReadKey();
+		}
+	}
+}
---
+++ thirteenth_homework_files/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+/*
+Взаимодействие с файловой системой.
+
+1.	В файле записана непустая последовательность целых чисел,
+являющихся числами Фибоначчи. Приписать еще столько же чисел этой последовательности.
+2.	Сложить два целых числа А и В.
+-Входные данные:
+В единственной строке входного файла INPUT.TXT записано два натуральных числа через пробел.
+-Выходные данные:
+В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел А и В.
+*/
+namespace thirteenth_homework_files
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("\n----------------------1---------------------\n");
+			string fibonacciFirstText = "0, 1, 1, 2, 3, 5, ";
+			string fibonacciSecondText = "8, 13, 21, 34, 55, 89";
+			string path = "text.txt";
+
+			string result = "";
+			if (!File.Exists(path))
+			{
+				var stream = File.Create(path);
+				stream.Close();
+			}
+			else
+			{
+				//то удаляем и заново создаем
+				FileInfo temp = new FileInfo(path);
+				temp.Delete();
+				var stream = File.Create(path);
+				stream.Close();
+			}
+
+			using (FileStream fileStream = new FileStream(path, FileMode.Open))
+			{
+				byte[] data = Encoding.UTF8.GetBytes(fibonacciFirstText);
+				fileStream.Write(data, 0, data.Length);
+			}
+
+			using (FileStream fileStream = File.OpenRead(path))
+			{
+				byte[] buffer = new byte[fileStream.Length];
+				fileStream.Read(buffer, 0, buffer.Length);
+
+				result += Encoding.UTF8.GetString(buffer);
+				Console.WriteLine($"первая часть: {result}");
+			}
+
+			//File.AppendAllText(path, fibonacciSecondText);
+			using (StreamWriter writer = File.AppendText(path))
+			{
+				writer.WriteLine(fibonacciSecondText);
+				result += fibonacciSecondText;
+			}
+			Console.WriteLine($"всё: {result}");
+			
+
+			Console.WriteLine("\n----------------------2---------------------\n");
+
+			int firstNumber = 3;
+			int secondNumber = 5;
+
+			string input = "Input.txt";
+			string output = "Output.txt";
+
+			//инициализация input.txt
+			if (!File.Exists(input))
+			{
+				var stream = File.Create(input);
+				stream.Close();
+			}
+			else
+			{
+				FileInfo temp = new FileInfo(input);
+				temp.Delete();
+				var stream = File.Create(input);
+				stream.Close();
+			}
+			
+			using (StreamWriter fileWriter = new StreamWriter(input))
+			{
+				string inputText = $"{firstNumber} {secondNumber}";
+				fileWriter.WriteLine(inputText);
+			}
+			//выведем из файла input.txt строку
+			string twoNumbers = null;
+			using (StreamReader fileReader = new StreamReader(input))
+			{
+				twoNumbers = fileReader.ReadToEnd();
+				Console.WriteLine($"Вывод 2-ух чисел из файла Input.txt в виде строки: {twoNumbers}");
+			}
+
+			//Превратим строку в List<int>:
+			List<int> numbers = new List<int>();
+			Console.Write("наши числа в int: ");
+			for (int i = 0; i < twoNumbers.Length; i++)
+			{
+				if (Char.IsNumber(twoNumbers[i]))
+				{
+					int numberToInt = (int)Char.GetNumericValue(twoNumbers[i]);
+					//или:
+					//int numberToInt = Convert.ToInt32(twoNumbers[i] - '0');
+
+					numbers.Add(numberToInt);
+					Console.Write(numberToInt +" ");
+				}
+			}
+			var sum = numbers.Sum();
+
+			Console.WriteLine($"\n\nсумма равна {sum}, затем запись в файл Output.txt");
+
+			//инициализация output.txt
+			if (!File.Exists(output))
+			{
+				var stream = File.Create(output);
+				stream.Close();
+			}
+			else
+			{
+				FileInfo temp = new FileInfo(output);
+				temp.Delete();
+				var stream = File.Create(output);
+				stream.Close();
+			}
+
+			using (StreamWriter fileWriter = new StreamWriter(output))
+			{
+				string outputText = $"{sum.ToString()}";
+				fileWriter.WriteLine(outputText);
+			}
+			using (StreamReader fileReader = new StreamReader(output))
+			{
+				string sumNumber = fileReader.ReadToEnd();
+				Console.WriteLine($"\nВывод из файла Output.txt: {sumNumber}");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
